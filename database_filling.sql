b15719fd7ee7b79002472b06f77f844c2679281f
wolf-null <57221440+wolf-null@users.noreply.github.com>
2022-02-04 19:53:05 +0300

    database fill

--- database_filling.sql
+++ database_filling.sql
@@ -1,11 +1,13 @@
-delete from Items;
-delete from Projects;
-delete from DomainStructure;
+delete from nodes;
+delete from projects_in_domain;
+delete from projects;
+delete from subdomain_structure;
+delete from domains;
 
-alter sequence items_id_seq restart with 1;
-alter sequence domainstructure_id_seq restart with 1;
+alter sequence nodes_id_seq restart with 1;
+alter sequence projects_id_seq restart with 1;
 
-insert into Nodes(name, ip)
+insert into nodes(name, ip)
 (
     /*The trick is not to add the already existed elements (not by id)*/
     /*Conflict isn't used since Nodes.id might differ*/
@@ -25,14 +27,72 @@ insert into Nodes(name, ip)
 
 --insert into Items(name) values ('awoo');
 
-insert into Projects(name, description)
+insert into domain_roles(domain_role)
     values
-    ('AwooL3', 'VRF between Durham, Chertsey and Seaham'),
-    ('Ch',     'Pseudo-L2 Durham & Chertsey');
+    ('datacenters'),
+    ('networks'),
+    ('network-groups');
 
-reindex schema public;
+insert into domains(name, domain_role)
+    values
+    ('ElephantAndCastle', role_by_name('datacenters')),
+    ('Durham', role_by_name('datacenters')),
+    ('Chertsey', role_by_name('datacenters')),
+    ('Seaham', role_by_name('datacenters')),
+    ('Channel-A', role_by_name('networks')),
+    ('Channel-B', role_by_name('networks')),
+    ('Channel-C', role_by_name('networks')),
+    ('Channel-D', role_by_name('networks')),
+    ('Services-for-Alice', role_by_name('network-groups')),
+    ('Services-for-Bob',   role_by_name('network-groups'));
 
-insert into DomainStructure(parent, child) values
-    (8,1);
-    --((select id from Items where id=8), ((select id from Items where id=1)));
+insert into subdomain_structure(parent_domain, child_domain)
+    values
+    (domain_by_name('Services-for-Alice'), domain_by_name('Channel-A')),
+    (domain_by_name('Services-for-Alice'), domain_by_name('Channel-B')),
+    (domain_by_name('Services-for-Alice'), domain_by_name('Channel-C')),
+    (domain_by_name('Services-for-Bob'), domain_by_name('Channel-D'));
+
+
+insert into projects(name, description)
+    values
+    ('AwooL3',    'VRF between Durham, Chertsey and Seaham'),
+    ('TheCh',     'Pseudo-L2 Durham & Chertsey');
+
+insert into projects_in_domain(project, domain)
+    values
+    (proj_by_name('AwooL3'), domain_by_name('Services-for-Alice')),
+    (proj_by_name('TheCh'), domain_by_name('Services-for-Bob'));
+
+/*insert into nodes_in_domain(node, domain)*/
+
+    /*with channels_readable(ar, br, chr) as
+    (
+        values
+        ('ElephantAndCastle-CCR1036',   'Durham-RB3011',    'Channel-A'),
+        ('ElephantAndCastle-CCR1036',   'Chertsey-RB2011',  'Channel-B'),
+        ('ElephantAndCastle-CCR1036',   'Seaham',           'Channel-C'),
+        ('Durham-RB3011',               'Chertsey-RBLHGR',  'Channel-D'),
+        ('Chertsey-RBLHGR',             'Chertsey-RB2011',  'Channel-D')
+    ),
+     */
+
+
+    /*
+     first_row as (select ar as nd from channels_readable),
+    second_row as (select br as nd from channels_readable),
+    both_rows as (select * from first_row union select * from second_row),
+    nodes_used(node) as (select distinct nd from both_rows)
+     */
+
+
+
+
+
+/*
+select domains.name from
+    domains left join domain_roles
+    on domains.domain_role = domain_roles.id where domain_roles.domain_role='networks';
+*/
+/*Now, assume, some particukla*/
 
--- database_structure.sql
+++ database_structure.sql
@@ -1,17 +1,29 @@
 ------------------------------------------------------ CLEANUP ---------------------------------------------------------
-drop table if exists domains;
-drop table if exists subdomain_structure;
-drop table if exists nodes;
+drop table if exists domains cascade ;
+drop table if exists subdomain_structure cascade ;
+drop table if exists nodes cascade ;
 drop table if exists nodes_in_domain;
-drop table if exists projects;
-drop table if exists projects_in_domain;
-
+drop table if exists projects cascade ;
+drop table if exists projects_in_domain cascade ;
+drop table if exists domain_roles cascade ;
+drop table if exists domains;
 ------------------------------------------------------ DOMAINS ---------------------------------------------------------
+create table domain_roles
+(
+    id                  serial,
+    domain_role         varchar(32),
+
+    primary key (id)
+);
+
 create table domains
 (
     id                  serial,
     name                varchar(128),
-    primary key (id)
+    domain_role         integer,
+
+    primary key (id),
+    foreign key (domain_role) references domain_roles(id)
 );
 
 /*Domains might have subdomains*/
@@ -56,6 +68,8 @@ create table projects
 (
     id                  serial,
     name                varchar(64),
+    description         varchar(128),
+    income              decimal,
 
     primary key (id)
 );
@@ -73,3 +87,22 @@ create table projects_in_domain
 
 ---------------------------------------------------- FUNCTIONS ---------------------------------------------------------
 
+create or replace function node_by_name(varchar) returns integer as
+$$
+    select id from nodes where nodes.name = $1 fetch first row only
+$$ language sql;
+
+create or replace function proj_by_name(varchar) returns integer as
+$$
+    select id from projects where projects.name = $1 fetch first row only
+$$ language sql;
+
+create or replace function role_by_name(varchar) returns integer as
+$$
+    select id from domain_roles where domain_roles.domain_role = $1 fetch first row only
+$$ language sql;
+
+create or replace function domain_by_name(varchar) returns integer as
+$$
+    select id from domains where domains.name = $1 fetch first row only
+$$ language sql;
